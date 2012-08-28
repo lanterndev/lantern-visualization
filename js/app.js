692030b252bd333bafcf3d87915a45fd64d44327
Javier Arce <javierarce@gmail.com>
2012-08-28 16:14:06 +0200

    Fixes styles

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 function drawP(svg, p1, p2) {
 
   var
+  map,
+  svg,
+  projection,
   p3      = { x: (p2.x - p1.x) / 2, y: 300 },
   w       = 350,
   h       = 200,
@@ -63,30 +66,86 @@ function drawP(svg, p1, p2) {
   }
 }
 
+function addPoints() {
+
+  d3.csv('data/centroids.csv', function(json) {
+
+    map
+    .append("g")
+    .selectAll("glow")
+    .data(json)
+    .enter()
+    .append("circle")
+    .attr("class", function(d) {
+      if (d.ISO3136 == selectedISO) return "hollow";
+      else return "glow";
+    })
+    .attr("filter", function(d) {
+
+      if (d.ISO3136 == selectedISO) return "url(#lightBlur)"
+      else return "url(#strongBlur)"
+
+    })
+    .attr("fill", function(d) {
+      if (d.ISO3136 == selectedISO) return "#FF6666";
+      else return "#fff";
+    })
+    .attr('cx', function(d, i) { return projection([d.LONG, d.LAT])[0]; } )
+    .attr('cy', function(d, i) { return projection([d.LONG, d.LAT])[1]; } )
+    .attr('r',  function(d, i) {
+      if (d.ISO3136 == selectedISO) return 6;
+      else return 5;
+    })
+
+    map
+    .append("g")
+    .selectAll("circle")
+    .data(json)
+    .enter()
+    .append("circle")
+    .attr("filter", "url(#lightBlur)")
+    .attr("stroke", function(d) {
+      if (d.ISO3136 == selectedISO) return "#FF6666";
+    })
+    .attr("fill", function(d) {
+      if (d.ISO3136 == selectedISO) return "none";
+      else return "#FFF";
+    })
+    .attr('cx', function(d, i) { return projection([d.LONG, d.LAT])[0]; } )
+    .attr('cy', function(d, i) { return projection([d.LONG, d.LAT])[1]; } )
+    .attr('r',  function(d, i) {
+      if (d.ISO3136 == selectedISO) return 2;
+      else return .5;
+    })
+  });
+}
+
 function start() {
 
   selectedISO = 'BR';
 
-  var projection = d3.geo.mercator()
+  projection = d3.geo.mercator()
   .scale(500)
   .translate([240, 300]);
 
-  var svg = d3.select("#canvas").append("svg");
+  svg = d3.select("#canvas").append("svg");
 
 
-  var filter = svg.append("svg:defs")
+  // Light blur
+  svg.append("svg:defs")
   .append("svg:filter")
-  .attr("id", "blur2")
+  .attr("id", "lightBlur")
   .append("svg:feGaussianBlur")
   .attr("stdDeviation", .7);
 
-  var filter = svg.append("svg:defs")
+  // Strong blur
+  svg.append("svg:defs")
   .append("svg:filter")
-  .attr("id", "blur")
+  .attr("id", "strongBlur")
   .append("svg:feGaussianBlur")
-  .attr("stdDeviation", .9);
+  .attr("stdDeviation", 2.5);
 
-  var map = svg
+  map = svg
   .append("g").attr("class", "map");
 
   d3.json('data/countries.json', function(json) {
@@ -97,54 +156,7 @@ function start() {
     .append("path")
     .attr("d", d3.geo.path().projection(projection));
 
-    d3.csv('data/centroids.csv', function(json) {
-
-      map
-      .append("g")
-      .selectAll("glow")
-      .data(json)
-      .enter()
-      .append("circle")
-      .attr("class", function(d) {
-        if (d.ISO3136 == selectedISO) return "hollow";
-        else return "glow";
-      })
-      .attr("filter", function(d) {
-        return "url(#blur)"
-      })
-      .attr("fill", function(d) {
-        if (d.ISO3136 == selectedISO) return "#FF6666";
-        else return "#fff";
-      })
-      .attr('cx', function(d, i) { return projection([d.LONG, d.LAT])[0]; } )
-      .attr('cy', function(d, i) { return projection([d.LONG, d.LAT])[1]; } )
-      .attr('r',  function(d, i) {
-        if (d.ISO3136 == selectedISO) return 6;
-        else return 5;
-      })
-
-      map
-      .append("g")
-      .selectAll("circle")
-      .data(json)
-      .enter()
-      .append("circle")
-      .attr("filter", "url(#blur2)")
-      .attr("stroke", function(d) {
-        if (d.ISO3136 == selectedISO) return "#FF6666";
-      })
-      .attr("fill", function(d) {
-        if (d.ISO3136 == selectedISO) return "none";
-        else return "#fff";
-      })
-      .attr('cx', function(d, i) { return projection([d.LONG, d.LAT])[0]; } )
-      .attr('cy', function(d, i) { return projection([d.LONG, d.LAT])[1]; } )
-      .attr('r',  function(d, i) {
-        if (d.ISO3136 == selectedISO) return 2;
-        else return 1;
-      })
-    });
-
+    addPoints();
 
   });
 
