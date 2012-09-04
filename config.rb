cf6b2e9adbfd547cdf0de01fddfb4c4875c3c823
Javier Arce <javierarce@gmail.com>
2012-09-04 17:19:24 +0200

    Removes old files

--- config.rb
+++ config.rb
@@ -1,16 +1,17 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "scss"
-images_dir = "img"
+http_path       = "/"
+css_dir         = "css"
+sass_dir        = "scss"
+images_dir      = "img"
 javascripts_dir = "js"
 
 relative_assets = true
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+
 output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
